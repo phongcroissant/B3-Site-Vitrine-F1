62f3ce582b572a6886995628f3670cb8893a13e2
phongnguyen <phongnguyen25000@gmail.com>
2025-09-25 16:52:11 +0200

    modification vite.config.js et suppression vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     tailwindcss()
   ],
-  base:'/'
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        circuit: resolve(__dirname, 'public/circuit.html'),
+        driverStanding: resolve(__dirname, 'public/driverStanding.html'),
+        pilotes: resolve(__dirname, 'public/pilotes.html'),
+      }
+    }
+  }
 });
