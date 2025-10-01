09ec1d9dfcb1d90c062a635e2032354e5c05e873
phongnguyen <phongnguyen25000@gmail.com>
2025-10-01 15:06:57 +0200

    modification dossier

--- src/js/driverStanding.js
+++ src/js/driverStanding.js
@@ -3,6 +3,11 @@
         const reponse = await fetch('https://api.jolpi.ca/ergast/f1/current/driverStandings.json')
         const driverStanding = await reponse.json()
         console.log(driverStanding)
+        const cardDrivers=document.querySelector("#drivers-card")
+        cardDrivers.innerHTML = driverStanding.MRData.StandingsTable.StandingsLists[0].DriverStandings
+            .map(driver => `
+              
+              `)
         
     } catch (error) {
         console.log(error)
--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,16 @@ import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss()
-  ],
+  root: '.', // racine du projet
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        circuit: resolve(__dirname, 'public/circuit.html'),
-        driverStanding: resolve(__dirname, 'public/driverStanding.html'),
-        pilotes: resolve(__dirname, 'public/pilotes.html'),
+        circuit: resolve(__dirname, 'src/pages/circuit.html'),
+        pilotes: resolve(__dirname, 'src/pages/pilotes.html'),
+        driverStanding: resolve(__dirname, 'src/pages/driverStanding.html')
       }
-    }
+    },
+    outDir: 'dist'
   }
-});
+})
