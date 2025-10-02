1f9e4344f77f35cc1374e9025d9a49c0e8bb375b
phongnguyen <phongnguyen25000@gmail.com>
2025-10-02 11:36:35 +0200

    ajout header dans vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         circuit: resolve(__dirname, 'src/pages/circuit.html'),
         pilotes: resolve(__dirname, 'src/pages/pilotes.html'),
-        driverStanding: resolve(__dirname, 'src/pages/driverStanding.html')
+        driverStanding: resolve(__dirname, 'src/pages/driverStanding.html'),
+        header: resolve(__dirname, 'src/__parts/header.html')
       }
     }
   }
