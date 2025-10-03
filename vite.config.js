501b9476c81f8792497cc47136342f7bedf5fcac
phongnguyen <phongnguyen25000@gmail.com>
2025-10-03 14:06:53 +0200

    test lien header

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
+import handlebars from 'vite-plugin-handlebars'
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(),
+    handlebars({
+      partialDirectory: resolve('./src/__parts')
+    })
+  ],
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         circuit: resolve(__dirname, 'src/pages/circuit.html'),
         pilotes: resolve(__dirname, 'src/pages/pilotes.html'),
-        driverStanding: resolve(__dirname, 'src/pages/driverStanding.html'),
-        header: resolve(__dirname, 'src/__parts/header.html')
+        driverStanding: resolve(__dirname, 'src/pages/driverStanding.html')
       }
     }
-  }
+  },
+  base:'/'
 })
