3d467391a6902501a2403f84757866c3b3ec24c5
Phong NGUYEN <phongnguyen25000@gmail.com>
2025-12-18 11:34:41 +0100

    delete: handlebars useless vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [tailwindcss(),
-    handlebars({
-      partialDirectory: resolve('./src/__parts')
-    }),
     react()
   ],
   base:'/'
