dd828c71f57a86eec8e581a75222968f75fee05e
Phong NGUYEN <phongnguyen25000@gmail.com>
2025-12-18 11:33:26 +0100

    delete: useless build vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,17 +11,5 @@ export default defineConfig({
     }),
     react()
   ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        races: resolve(__dirname, 'src/pages/races.html'),
-        pilotes: resolve(__dirname, 'src/pages/pilotes.html'),
-        driverStanding: resolve(__dirname, 'src/pages/driverStanding.html'),
-        constructorStanding: resolve(__dirname,'src/pages/constructorStanding.html'),
-        resultatRace: resolve(__dirname,'src/pages/resultatRace.html')
-      }
-    }
-  },
   base:'/'
 })
