8b1b9de3e99f7bb7497e54464e531dd272ffeef1
phongnguyen <phongnguyen25000@gmail.com>
2026-05-04 17:44:38 +0200

    add:login

---
+++ src/components/pages/Login.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react"
+import { supabase } from "../../lib/supabase"
+import { useNavigate } from "react-router-dom"
+
+export default function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const navigate = useNavigate()
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    setError(null)
+    setLoading(true)
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+
+    if (error) {
+      setError(error.message)
+      setLoading(false)
+      return
+    }
+
+    console.log("USER CONNECTED:", data.user)
+
+    setLoading(false)
+
+    // redirection après login
+    navigate("/")
+  }
+
+  return (
+    <form onSubmit={handleLogin}>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+
+      <input
+        type="password"
+        placeholder="Mot de passe"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+
+      <button type="submit" disabled={loading}>
+        {loading ? "Connexion..." : "Se connecter"}
+      </button>
+    </form>
+  )
+}
