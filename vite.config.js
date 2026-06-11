8e4a1e708c4fbaa24ed49800c228d0debb2085ab
phongnguyen <phongnguyen25000@gmail.com>
2026-06-11 14:46:44 +0200

    feat:add eslint

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
-import handlebars from 'vite-plugin-handlebars'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
