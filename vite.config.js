50be80ce14c969e3ed9f98044ccee52089072896
Phong NGUYEN <phongnguyen25000@gmail.com>
2026-06-14 13:38:39 +0200

    ajout import expect and test

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: './src/setupTests.js',
+    setupFiles: './src/setupTests.jsx',
   },
   base: '/'
 })
